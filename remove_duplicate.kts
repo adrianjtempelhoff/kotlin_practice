b3a3d5ccd3906957ec772250879f87bfb1c46a7c
A.J. Tempelhoff <ajtempelhoff@gmail.com>
2021-05-13 19:06:08 +0200

    remove duplicates in list
    convert list to set

---
+++ remove_duplicate.kts
@@ -0,0 +1,8 @@
+fun main() {
+  val animals = listOf("apple", "biscuit", "apple", "cat", "cat", "cat", "dog", "elephant", "fox", "goat", "elephant")
+  println("$animals")
+  
+  println("list to set = ${animals.toSet()}")
+}
+
+main()
