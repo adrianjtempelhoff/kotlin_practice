a2bc9431476b4e951ce3afbd5f035545020e179d
A.J. Tempelhoff <ajtempelhoff@gmail.com>
2021-05-13 09:04:52 +0200

    added some testing code using the if expression

---
+++ if_expression.kts
@@ -0,0 +1,29 @@
+val bigger = 101
+val smaller = 1
+val max = if (bigger > smaller) bigger else smaller
+
+if (bigger > 10) {
+  println("Greater than 10!")
+} else if (bigger > 100) {
+  println("Greater than 100!")
+} else {
+  println("Not too big.")
+}
+
+val number = 121
+val bucket: Any = if (number > 100) {
+  "alpha"
+} else if (number > 90) {
+  println("Less than 90")
+  2
+} else if (number > 80) {
+  1
+} else {
+  0
+}
+
+println("bucket = $bucket")
+
+val isGreater = number > 100
+
+
