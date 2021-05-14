8f1ad43e2db79b2b291bca250c6dd1d8b6730a10
adrianjtempelhoff <ajtempelhoff@gmail.com>
2021-05-14 12:16:02 +0200

    added more lamba function examples

--- lamba_functions.kts
+++ lamba_functions.kts
@@ -1,7 +1,19 @@
 fun main() {
   val lam: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
   println("Sum = ${lam(5, 10)}")
+  
+  val l = { a: Int, b: Int -> a * b }
+  println("5 * 10 = ${l(5, 10)}")
+  println("10 * 50 = ${l.invoke(10, 50)}")
+  
+  val num = listOf(1, 5, 7, 9, 10, 13)
+  println(num.filter { it < 7 })
+  
+  val greet: String.() -> String = { "Hey ya! $this" }
+  println("Jim".greet())
+  
 }
 
 main()
+
 
