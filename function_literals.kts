cb4ff1563dc616b170b2d63db5126704a3b29fe9
adrianjtempelhoff <ajtempelhoff@gmail.com>
2021-05-26 08:18:07 +0200

    add function literals example

---
+++ function_literals.kts
@@ -0,0 +1,12 @@
+
+fun main() {
+  val area = fun(base: Int, height: Int): Int {return (base * height) / 2}
+
+  println(area(15, 19))
+
+  val perimeter = {side1: Int, side2: Int -> (side1 + side2)*2}
+
+  println(perimeter(15,24))
+}
+
+main()
